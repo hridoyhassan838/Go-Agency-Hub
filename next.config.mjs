4571618308944b5d008b2fde0a385c1796c48fdd
MD.HRIDOY HASSAN <freelancerbd838@gmail.com>
2025-08-16 17:32:35 +0000

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    allowedDevOrigins: [
+      "ebc4783f-fe12-4aec-8d28-201f22514c67-00-2xcv1smm28b7s.pike.replit.dev"
+    ],
+};
+
+export default nextConfig;
